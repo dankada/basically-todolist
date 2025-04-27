ba8b5e9103b3e1f84ca8ffe05cb270927a382edb
John Doe <josh.breve@gmail.com>
2025-04-27 16:05:51 +0800

    Initial commit - added Java files, such as Main.java. Included also a short funny message haha

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // Basically, we're mooming now
+        System.out.println("AHH TO DO LIST AHHHHHH");
+        // Lloyd speaking, we should really learn how to use commentss HAHAHAHHAHAHA
+    }
+}
