03967ab4702ee9bdd2e51a0c06e4f9d5eb0b77ff
pret-zels <fvvlabor@addu.edu.ph>
2025-05-12 21:49:27 +0800

    Add files via upload

---
+++ Entry.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.finalproject;
+
+public abstract class Entry {
+
+    protected int id;
+    protected String header;
+    protected String description;
+
+    public Entry(int id, String header, String description) {
+        this.id = id;
+        this.header = header;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public void setHeader(String header) {
+        this.header = header;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public abstract String getDetails();
+}
--- Task.java
+++ Task.java
@@ -2,33 +2,54 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.comprog_finalproject;
+package com.mycompany.finalproject;
 
-/**
- *
- * @author Fritz Ver
- */
-public class Task {
-    int id;
-    String group;
-    boolean isCompleted;
-    String dueDate; 
-    String header;
-    String description;
+public class Task extends Entry {
+    private String group;
+    private boolean isCompleted;
+    private String dueDate;
 
     public Task(int id, String group, boolean isCompleted, String dueDate, String header, String description) {
-        this.id = id;
+        super(id, header, description);
         this.group = group;
         this.isCompleted = isCompleted;
         this.dueDate = dueDate;
-        this.header = header;
-        this.description = description;
     }
 
     public String getStatusIcon() {
         return isCompleted ? "/" : "X";
     }
 
+    public String getDetails() {
+        return String.format("Group: %s\nStatus: %s\nDue Date: %s\nHeader: %s\nDescription: %s",
+                group, getStatusIcon(), dueDate, header, description);
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public boolean isIsCompleted() {
+        return isCompleted;
+    }
+
+    public void setIsCompleted(boolean isCompleted) {
+        this.isCompleted = isCompleted;
+    }
+
+    public String getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(String dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    @Override
     public String toString() {
         return String.format("%d %s %s %s %s", id, group, getStatusIcon(), dueDate, header);
     }
